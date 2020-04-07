c371ebfd70497488b87b8e517d73217e61ef0649
DazWilkin <daz.wilkin@gmail.com>
2020-04-07 14:03:43 -0700

    Added "grafeas" to the build by way of test

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
+use git2::Repository;
+use std::path::Path;
+
 fn main() {
+    Repository::clone(
+        "https://github.com/googleapis/googleapis.git",
+        Path::new("googleapis"),
+    )
+    .expect("failed to clone");
+
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
         out_dir: "src/google/api",
         includes: &["googleapis"],
@@ -18,6 +27,27 @@ fn main() {
             "googleapis/google/rpc/code.proto",
             "googleapis/google/rpc/error_details.proto",
             "googleapis/google/rpc/status.proto",
+        ],
+        rust_protobuf: true,
+        ..Default::default()
+    })
+    .expect("protoc");
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: "src/grafeas/v1",
+        includes: &["googleapis", "protoc-3.11.4-linux-x86_64/include"],
+        input: &[
+            "googleapis/grafeas/v1/attestation.proto",
+            "googleapis/grafeas/v1/build.proto",
+            "googleapis/grafeas/v1/common.proto",
+            "googleapis/grafeas/v1/cvss.proto",
+            "googleapis/grafeas/v1/deployment.proto",
+            "googleapis/grafeas/v1/discovery.proto",
+            "googleapis/grafeas/v1/grafeas.proto",
+            "googleapis/grafeas/v1/image.proto",
+            "googleapis/grafeas/v1/package.proto",
+            "googleapis/grafeas/v1/provenance.proto",
+            "googleapis/grafeas/v1/upgrade.proto",
+            "googleapis/grafeas/v1/vulnerability.proto",
         ],
         rust_protobuf: true,
         ..Default::default()
