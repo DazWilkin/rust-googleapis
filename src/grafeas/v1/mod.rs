47eb3b0854b637bda0fa06336ba928fc68eebe47
DazWilkin <daz.wilkin@gmail.com>
2020-04-07 14:04:36 -0700

    Added grafeas by way of test. Working.

---
+++ src/grafeas/v1/mod.rs
@@ -0,0 +1,14 @@
+pub use crate::google::rpc::status;
+
+pub mod attestation;
+pub mod build;
+pub mod common;
+pub mod cvss;
+pub mod deployment;
+pub mod discovery;
+pub mod grafeas;
+pub mod image;
+pub mod package;
+pub mod provenance;
+pub mod upgrade;
+pub mod vulnerability;
